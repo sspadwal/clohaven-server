88624e40b0e37f1c3fc03232873899766fb9f190
bk-birla <bkbirla1002@gmail.com>
2025-03-31 16:39:36 +0530

    added frontend url into app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-    origin: ['https://clohaven.vercel.app/', 'http://localhost:5173'], // Add both ports
+    origin: ['https://clohaven.vercel.app', 'http://localhost:5173'], // Add both ports
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
