7d26418f9028d4e9adbe97f60462c0aa67fded9f
bk-birla <bkbirla1002@gmail.com>
2025-03-31 15:06:08 +0530

    Fix MongoDB connection options

--- config/db.js
+++ config/db.js
@@ -15,8 +15,7 @@ async function connectDB() {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true, // Use the new URL parser
             useUnifiedTopology: true, // Use the new connection management engine
-            useFindAndModify: false, // Disable findAndModify deprecation warnings
-            useCreateIndex: true, // Enable createIndex to avoid deprecation warnings
+            // Removed deprecated options 'useFindAndModify' and 'useCreateIndex'
         });
 
         console.log('MongoDB connected successfully!');
