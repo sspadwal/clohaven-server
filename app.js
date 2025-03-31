3e012a24286831eeee0d026971633dcd82fddefb
bk-birla <bkbirla1002@gmail.com>
2025-03-31 15:09:33 +0530

    added common path to check the server

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use('/api/admin', adminRoutes);
 app.use('/api/categories', categoryRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.status("hello world this is backend ")
+})
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
